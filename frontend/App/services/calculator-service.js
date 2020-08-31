7e2ff6daa837f96620a391cf00d4cc082a5b5ea8
Jeremy Gibson <jgibsongt21@gmail.com>
2020-08-31 19:01:35 -0400

    requests added

--- frontend/App/services/calculator-service.js
+++ frontend/App/services/calculator-service.js
@@ -1,12 +1,39 @@
-const axios = require('axios');
+const axios = require("axios");
 
-const uriBase = 'https://cs4261-pa1.herokuapp.com';
+const uriBase = "https://cs4261-pa1.herokuapp.com";
 
 exports.add = async (previous, current) => {
   const response = await axios.post(`${uriBase}/add`, {
     num1: previous,
-    num2: current
+    num2: current,
   });
 
   return response.data.result;
-}
+};
+
+exports.subtract = async (previous, current) => {
+  const response = await axios.post(`${uriBase}/subtract`, {
+    num1: previous,
+    num2: current,
+  });
+
+  return response.data.result;
+};
+
+exports.multiply = async (previous, current) => {
+  const response = await axios.post(`${uriBase}/multiply`, {
+    num1: previous,
+    num2: current,
+  });
+
+  return response.data.result;
+};
+
+exports.divide = async (previous, current) => {
+  const response = await axios.post(`${uriBase}/divide`, {
+    num1: previous,
+    num2: current,
+  });
+
+  return response.data.result;
+};
